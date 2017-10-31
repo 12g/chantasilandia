c695d3ffbcb38fb21f12a6c6b3d113b06a3590bc
Benjamin <Benjamin@BENJAWIN7>
2017-10-31 00:18:26 -0300

    Clase Cuenta asociada al Entity framework

--- Juegos.Negocio/Cuenta.cs
+++ Juegos.Negocio/Cuenta.cs
@@ -8,6 +8,111 @@ namespace BibliotecaClases
 {
     public class Cuenta
     {
+        private int _id;
+        private string _usuario;
+        private string _contraseña;
         
+        public int Id { get => _id; set => _id = value; }
+        public string Usuario { get => _usuario; set => _usuario = value; }
+        public string Contraseña { get => _contraseña; set => _contraseña = value; }
+
+        public Cuenta()
+        {
+            this.Init();
+        }
+
+        public Cuenta(int id, string usuario, string contraseña)
+        {
+            this.Id = id;
+            this.Usuario = usuario;
+            this.Contraseña = contraseña;
+        }
+
+        private void Init()
+        {
+            this.Id = -1;
+            this.Usuario = "";
+            this.Contraseña = "";
+        }
+
+        public bool Crear()
+        {
+            try
+            {
+                Juegos.DALC.Cuentas cuenta = new Juegos.DALC.Cuentas();
+
+                cuenta.cuentaID = this.Id;
+                cuenta.cuentaUsuario = this.Usuario;
+                cuenta.cuentaPassword = this.Contraseña;
+
+                CommonBC.Modelo.Cuentas.Add(cuenta);
+
+                CommonBC.Modelo.SaveChanges();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete()
+        {
+            try
+            {
+                var cuenta = CommonBC.Modelo.Cuentas.First
+                    (aux => aux.cuentaID == this.Id);
+
+                CommonBC.Modelo.Cuentas.Remove(cuenta);
+
+                CommonBC.Modelo.SaveChanges();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Update()
+        {
+            try
+            {
+                var cuenta = CommonBC.Modelo.Cuentas.First
+                    (aux => aux.cuentaID == this.Id);
+
+                cuenta.cuentaID = this.Id;
+                cuenta.cuentaUsuario = this.Usuario;
+                cuenta.cuentaPassword = this.Contraseña;
+                CommonBC.Modelo.SaveChanges();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool BuscarUno()
+        {
+            try
+            {
+                var cuenta = CommonBC.Modelo.Cuentas.First
+                    (aux => aux.cuentaID == this.Id);
+
+                this.Id = cuenta.cuentaID;
+                this.Usuario = cuenta.cuentaUsuario;
+                this.Contraseña = cuenta.cuentaPassword;
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
     }
 }
