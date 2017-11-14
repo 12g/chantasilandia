4a134e13bc53280f906507891a97e59056c1a99d
Benjamin <Benjamin@BENJAWIN7>
2017-11-14 20:02:35 -0300

    Servicio configurado para pruebas

--- Juegos.Servicio/Service1.svc.cs
+++ Juegos.Servicio/Service1.svc.cs
@@ -5,15 +5,13 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
-using Juegos.Negocio;
 using Juegos.Negocio.Modelo;
-using Juegos.Negocio.Colecciones;
 
 namespace Juegos.Servicio
 {
     // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
     // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
-    public class ServicioJuegos : IServicioJuegos
+    public class Service1 : IServicioJuegos
     {
 
         public bool CrearJuegoCasualServicio(string nombre, bool requiereSupervision, bool poseeCinturon)
